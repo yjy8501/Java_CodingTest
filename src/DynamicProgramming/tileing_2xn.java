b7b7dad4f2ad2dbe0cabdb9c62070a56faff266a
JaeYong <yjy850146@gmail.com>
2023-04-28 21:38:42 +0900

    백준 11726번 문제

---
+++ src/DynamicProgramming/tileing_2xn.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class tileing_2xn {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long[] dp = new long[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < dp.length; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
