d9202ffbd18063d970ba50c24e2a05836a2b8aba
JaeYong <yjy850146@gmail.com>
2023-06-10 00:29:49 +0900

    백준 11053번 문제

--- src/DynamicProgramming/Longest_Increasing_Subsequence.java
+++ src/DynamicProgramming/Longest_Increasing_Subsequence.java
@@ -3,8 +3,8 @@ package DynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+
 import java.util.StringTokenizer;
 
 public class Longest_Increasing_Subsequence {
@@ -15,28 +15,26 @@ public class Longest_Increasing_Subsequence {
         int N = Integer.parseInt(bf.readLine());
         // 수열을 입력받는다
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        // dp배열을 선언해준다
+        // dp 배열을 선언해준다
         int[] dp = new int[N+1];
-        int[] max = new int[]{0, 0};
+        int[] arr = new int[N+1];
 
+        for(int i = 1; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
         for(int i = 1; i < dp.length; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            // Max 보다 클 때 Max값 + 1
-            if(num >= max[0]) {
-                dp[i] = dp[max[1]] + 1;
-                max[0] = num;
-                max[1] = i;
-
-            }
-            // Max 보다 작을 때 자기 자신 값 또는 1
-            else {
-                dp[i] = Math.max(dp[i], 1);
+            dp[i] = Math.max(dp[i], 1);
+            // i보다 작은 값 중에 가장 큰 값
+            for(int j = 0; j < i; j++) {
+                if(arr[j] < arr[i] && dp[j] >= dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
             }
         }
 
-        System.out.println(dp[N]);
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+
     }
 }
 
